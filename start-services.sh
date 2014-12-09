38b8d05b9f86ba8bc790ac94a2fd3d74f7be0ab2
gitolite tester <tester@example.com>
2014-12-09 10:56:07 -0500

    added ubuntu and supervisor to the api docker image

--- start-services.sh
+++ start-services.sh
@@ -7,4 +7,16 @@ sudo docker run -d -p 5022:22 --name hivewing-images --link redis-dev:redis --li
 echo "Running hivewing.io/api"
 sudo docker rm -f hivewing-api
 sudo docker run -d -p 5000:3000 --name hivewing-api --link redis-dev:redis --link ddb-dev:ddb --link pg-dev:pg --link sqs-dev:sqs --link s3-dev:s3 --env-file container.env hivewing.io/api
-sudo docker run -i --name hivewing-api --link redis-dev:redis --link ddb-dev:ddb --link pg-dev:pg --link sqs-dev:sqs --link s3-dev:s3 --env-file container.env hivewing.io/api bash
+#sudo docker run -i --name hivewing-api --link redis-dev:redis --link ddb-dev:ddb --link pg-dev:pg --link sqs-dev:sqs --link s3-dev:s3 --env-file container.env hivewing.io/api bash
+
+echo "*********************************************************************************"
+echo "*********************************************************************************"
+echo "*********************************************************************************"
+echo " Images Server running ssh on "
+echo "     localhost:5022"
+echo ""
+echo " API Server running on "
+echo "     localhost:5000"
+echo "*********************************************************************************"
+echo "*********************************************************************************"
+echo "*********************************************************************************"
