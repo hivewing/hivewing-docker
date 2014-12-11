71fe2bca049238baa720bdbb6f7a540ec84138a2
gitolite tester <tester@example.com>
2014-12-11 17:20:09 -0500

    update, now push!

--- images/start-images.sh
+++ images/start-images.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Switch to the git user..
 export HIVEWING_GITOLITE_ROOT=/home/git/.gitolite
-export HIVEWING_GITOLITE_SHELL_COMMAND=/home/gitolite/bin/gitolite
-export HIVEWING_GITOLITE_REPOSITORIES_ROOT=/home/gitolite/repositories
+export HIVEWING_GITOLITE_SHELL_COMMAND=/home/git/bin/gitolite
+export HIVEWING_GITOLITE_REPOSITORIES_ROOT=/home/git/repositories
+export HOME=/home/git/
+export ROOT=/home/git
 
 exec java -jar /home/git/hivewing-images.jar
