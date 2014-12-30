a52641c5a36463d1f5659fea887a81e8f4bd5279
gitolite tester <tester@example.com>
2014-12-30 17:38:50 -0500

    change to remove redundant commented line

--- start-services.sh
+++ start-services.sh
@@ -3,7 +3,6 @@ echo "Running hivewing.io/images"
 sudo docker rm -f hivewing-images
 sudo docker run -d -p 5022:22 --name hivewing-images --link redis-dev:redis --link ddb-dev:ddb --link pg-dev:pg --link sqs-dev:sqs --link s3-dev:s3 --link s3-dev:hive-images.hivewing.io.s3 --env-file container.env hivewing.io/images
 #sudo docker run -i  -p 5022:22 --name hivewing-images --link redis-dev:redis --link ddb-dev:ddb --link pg-dev:pg --link sqs-dev:sqs --link s3-dev:s3 --env-file container.env hivewing.io/images bash
-#sudo docker run -i  -p 5022:22 --name hivewing-images --link redis-dev:redis --link ddb-dev:ddb --link pg-dev:pg --link sqs-dev:sqs --link s3-dev:s3 --env-file container.env hivewing.io/images bash
 
 echo "Running hivewing.io/api"
 sudo docker rm -f hivewing-api
