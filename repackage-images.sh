4d6b969a2eb923c4f4043d0ac43c461cd756361a
gitolite tester <tester@example.com>
2015-01-02 14:51:03 -0500

    added repackage images

---
+++ repackage-images.sh
@@ -0,0 +1,15 @@
+
+#! /bin/bash
+pushd .
+cd ../hivewing-images
+LEIN_SNAPSHOTS_IN_RELEASE=override lein uberjar
+popd
+
+pushd .
+cd images
+cp ../../hivewing-images/target/uberjar/*.uber.jar hivewing-images.uber.jar
+./rebuild.sh
+
+popd
+
+./start-core.sh
